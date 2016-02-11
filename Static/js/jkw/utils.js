39cdd62666ec53f5485fd877392a1b4bdf364dc9
jkwchunjae <jkwchunjae@daum.net>
2016-02-12 00:04:10 +0900

    블로그, 일기장
    블로그 폭 줄이고, 글씨 크기 키움.
    일기장 요일 표시

---
+++ Static/js/jkw/utils.js
@@ -0,0 +1,7 @@
+﻿function getDate8(date) {
+	return date.getFullYear() + '.' + padStr(date.getMonth() + 1) + '.' + padStr(date.getDate());
+}
+
+function padStr(value) {
+	return (value < 10) ? '0' + value : '' + value;
+}
