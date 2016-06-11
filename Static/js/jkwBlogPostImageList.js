9fdd019cdc08a3fb43bb870a523dbc5f9e820462
jkwchunjae <jkwchunjae@daum.net>
2016-06-11 22:36:43 +0900

    [Blog] 이미지 연속 보기 기능 추가

---
+++ Static/js/jkwBlogPostImageList.js
@@ -0,0 +1,29 @@
+﻿
+$(document).ready(function () {
+
+	function initImageList() {
+		var imageListIndex = 0;
+		$('div.image-list').each(function () {
+			$(this).attr('data-index', imageListIndex)
+			imageListIndex++
+
+			var imageIndex = 0;
+			$(this).find('img').each(function () {
+				$(this).attr('data-index', imageIndex)
+				imageIndex++
+			})
+
+			$(this).find('img[data-index="0"]').css('display', 'block')
+		})
+	}
+
+	initImageList()
+
+	$('.image-list img').click(function () {
+		var imageCount = $(this).parent().find('img').length
+		var index = parseInt($(this).attr('data-index'), 10)
+
+		$(this).css('display', 'none')
+		$('.image-list img[data-index="' + ((index + 1) % imageCount) + '"]').css('display', 'block')
+	})
+});
